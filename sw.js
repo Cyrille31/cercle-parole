8322e757a8a06e441a1469a32a94bd1dd1938203
Cyrille31 <cyrille.gindre.31@gmail.com>
2026-06-26 15:03:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cercle-v12';
+const CACHE = 'cercle-v13';
 const ASSETS = [
   '/cercle-parole/',
   '/cercle-parole/index.html',
@@ -7,14 +7,22 @@ const ASSETS = [
   '/cercle-parole/icons/icon-512.png',
   '/cercle-parole/icons/logo_cgexcel.jpg'
 ];
+
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ).then(() => self.clients.claim()));
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
+
+// Allow page to trigger immediate activation of new SW
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
